494b3ae46375922ec144597253dad3b41a90e4c0
GRIDNETOS <108996821+GRIDNETOS@users.noreply.github.com>
2025-12-24 02:18:45 +0500

    Add targetver.h

---
+++ GRIDNETCore/GRIDNET/targetver.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Do³¹czenie nag³ówka SDKDDKVer.h definiuje najwy¿sz¹ dostêpn¹ platformê systemu Windows.
+
+// Jeœli chcesz skompilowaæ aplikacjê dla wczeœniejszej platformy systemu Windows, do³¹cz nag³ówek WinSDKVer.h i
+// ustaw makro _WIN32_WINNT na platformê, któr¹ chcesz wspieraæ, przed do³¹czeniem nag³ówka SDKDDKVer.h.
+
+//#include <SDKDDKVer.h>
