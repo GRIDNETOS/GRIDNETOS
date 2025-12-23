43fa7eb9d27dad20270da055c20ae3d0e2f18c01
GRIDNETOS <108996821+GRIDNETOS@users.noreply.github.com>
2025-12-24 02:46:23 +0500

    Add cmemparam.cpp

---
+++ GRIDNETCore/GRIDNET/cmemparam.cpp
@@ -0,0 +1,6 @@
+#include "cmemparam.h"
+
+CMemParam::CMemParam(eParamType paramType, eMemType memType, eAccessType accessType, uint64_t memSize) : IMemParam(paramType, memType, accessType, memSize)
+{
+
+}
