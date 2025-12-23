27f81d99f13df09160f925e1d85abadc48f01cbe
GRIDNETOS <108996821+GRIDNETOS@users.noreply.github.com>
2025-12-24 02:48:48 +0500

    Add test_notify_peer_task.cpp

---
+++ GRIDNETCore/GRIDNET/Kademlia/test/unit_tests/test_notify_peer_task.cpp
@@ -0,0 +1,100 @@
+// Copyright (c) 2013-2014, David Keller
+// All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the University of California, Berkeley nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY DAVID KELLER AND CONTRIBUTORS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "common.hpp"
+#include "tracker_mock.hpp"
+#include "routing_table_mock.hpp"
+#include "task_fixture.hpp"
+
+#include <vector>
+#include <utility>
+
+#include "kademlia/id.hpp"
+#include "kademlia/peer.hpp"
+#include "kademlia/ip_endpoint.hpp"
+#include "kademlia/notify_peer_task.hpp"
+
+namespace {
+
+namespace k = kademlia;
+namespace kd = k::detail;
+
+using fixture = k::test::task_fixture;
+
+BOOST_AUTO_TEST_SUITE( notify_peer_task )
+
+BOOST_FIXTURE_TEST_SUITE( test_usage, fixture )
+
+BOOST_AUTO_TEST_CASE( can_query_known_peer_for_specific_id )
+{
+    kd::id const my_id{ "a" };
+    routing_table_.expected_ids_.emplace_back( my_id );
+    auto p1 = create_and_add_peer( "192.168.1.2", kd::id{ "1a" } );
+    auto p2 = create_and_add_peer( "192.168.1.3", kd::id{ "2a" } );
+    auto p3 = create_and_add_peer( "192.168.1.4", kd::id{ "4a" } );
+
+    // p1 doesn't know closer peer.
+    tracker_.add_message_to_receive( p1.endpoint_
+                                   , p1.id_
+                                   , kd::find_peer_response_body{} );
+
+    // p2 doesn't know closer peer.
+    tracker_.add_message_to_receive( p2.endpoint_
+                                   , p2.id_
+                                   , kd::find_peer_response_body{} );
+
+    // p3 doesn't know closer peer.
+    tracker_.add_message_to_receive( p3.endpoint_
+                                   , p3.id_
+                                   , kd::find_peer_response_body{} );
+
+
+    bool finished = false;
+    auto on_finish = [ &finished ] { finished = true; };
+
+    kd::start_notify_peer_task( my_id, tracker_, routing_table_, on_finish );
+
+    io_service_.poll();
+
+    // Task queried routing table to find closest known peers.
+    BOOST_REQUIRE_EQUAL( 1, routing_table_.find_call_count_ );
+
+    // Task asked p1, p2 & p3 for a closer peer or the value.
+    kd::find_peer_request_body const fv{ my_id };
+    BOOST_REQUIRE( tracker_.has_sent_message( p1.endpoint_, fv ) );
+    BOOST_REQUIRE( tracker_.has_sent_message( p2.endpoint_, fv ) );
+    BOOST_REQUIRE( tracker_.has_sent_message( p3.endpoint_, fv ) );
+
+    // Task didn't send any more message.
+    BOOST_REQUIRE( ! tracker_.has_sent_message() );
+    BOOST_REQUIRE( finished );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}
+
