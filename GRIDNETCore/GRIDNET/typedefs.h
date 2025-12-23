a2fdfc041c639b27536983a627bb598f824ad5f3
GRIDNETOS <108996821+GRIDNETOS@users.noreply.github.com>
2025-12-24 02:19:05 +0500

    Add typedefs.h

---
+++ GRIDNETCore/GRIDNET/typedefs.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "stdafx.h"
+typedef std::vector<unsigned char> bytes_t;
+typedef std::vector<unsigned char> secure_bytes_t;
+
+typedef std::vector<bytes_t> hashvector_t;
+typedef std::set<bytes_t> hashset_t;
+
+typedef std::string secure_string_t;
+
+typedef std::vector<int> ints_t;
+typedef std::vector<int> secure_ints_t;
