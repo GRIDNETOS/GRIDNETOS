372c11fe40286732423bf1c95b1f62300e0d9dd1
GRIDNETOS <108996821+GRIDNETOS@users.noreply.github.com>
2025-12-24 02:47:29 +0500

    Add stdafx.cpp

---
+++ GRIDNETCore/GRIDNET/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp: plik Ÿród³owy , który zawiera tylko standardowe zawarcia
+// GRIDNET.pch bêdzie wstêpnie skompilowanym nag³ówkiem
+// stdafx.obj bêdzie zawieraæ informacje typu wstêpnie skompilowanego
+
+#include "stdafx.h"
+
+// TODO: odwo³aj siê do dowolnych dodatkowych nag³ówków, których potrzebujesz w pliku STDAFX.H
+// a nie w tym pliku
