0378a10336c6a29416c6e1705c1a90514d327ceb
GRIDNETOS <108996821+GRIDNETOS@users.noreply.github.com>
2025-12-24 02:17:08 +0500

    Add cvalueparam.h

---
+++ GRIDNETCore/GRIDNET/cvalueparam.h
@@ -0,0 +1,12 @@
+#ifndef CVALUEPARAM_H
+#define CVALUEPARAM_H
+
+#include "../interfaces/ivalueparam.h"
+
+class CValueParam : public IValueParam
+{
+public:
+    CValueParam(eParamType paramType);
+};
+
+#endif // CVALUEPARAM_H
