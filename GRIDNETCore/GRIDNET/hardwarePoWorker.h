a110e51d3f247da9ede9335b73d8d4ffbafd5fa4
GRIDNETOS <108996821+GRIDNETOS@users.noreply.github.com>
2025-12-24 02:17:33 +0500

    Add hardwarePoWorker.h

---
+++ GRIDNETCore/GRIDNET/hardwarePoWorker.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "stdafx.h"
+#include <thread>
+#include "powjob.h"
+
+class CHardwarePoWorker
+{
+private:
+	size_t max_work_size;
+	size_t wsize;
+	size_t compute_shaders;
+
+	bool hasBitAlign;
+	bool hasOpenCL11plus;
+	bool hasOpenCL12plus;
+	cl_mem outputBuffer;
+	cl_context context;
+	cl_command_queue commandQueue;
+	cl_program program;
+	cl_mem outputBuffer;
+	cl_mem CLbuffer0;
+	cl_mem hash_buffer;
+public:
+};
